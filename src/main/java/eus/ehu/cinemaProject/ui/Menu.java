7c03d2e9a69a3ec05785e63f778f1b00f8f9562c
lrodriguez154 <lrodriguez154@ikasle.ehu.eus>
2025-03-25 15:19:43 +0100

    Add menu layout. Displays title and 2 buttons for Login and Registration use cases

--- src/main/java/eus/ehu/cinemaProject/ui/Menu.java
+++ src/main/java/eus/ehu/cinemaProject/ui/Menu.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class Menu extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Menu.class.getResource("signin.fxml")); //Menu --> SignIn
+        FXMLLoader fxmlLoader = new FXMLLoader(Menu.class.getResource("menuLayout.fxml")); //Menu --> SignIn
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("CineFlix");
         stage.setScene(scene);
         stage.show();
     }
