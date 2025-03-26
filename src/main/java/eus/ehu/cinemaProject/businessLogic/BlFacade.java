4cfa73269868094b5269e17c33f6fc79b3de27f4
Beñat <bercibengoa001@ikasle.ehu.eus>
2025-03-26 18:19:47 +0100

    signUp method added to the BlFacade interface

--- src/main/java/eus/ehu/cinemaProject/businessLogic/BlFacade.java
+++ src/main/java/eus/ehu/cinemaProject/businessLogic/BlFacade.java
@@ -4,17 +4,27 @@ import eus.ehu.cinemaProject.domain.users.User;
 /**
  * Interface that specifies the business logic.
  */
-public interface BlFacade  {
+public interface BlFacade {
 
     // ===== Define the public interface of the BL
 
     /**
      * Attempts to log in a user with the given credentials
-     * @param email the username
+     *
+     * @param email    the email
      * @param password the password
      * @return User if login successful, null otherwise
      */
     User login(String email, String password);
 
+    /**
+     * Registers a customer with the given credentials and personal data
+     *
+     * @param email    the email
+     * @param password the password
+     * @param name     the name
+     * @param surname  the surname
+     */
+    void signUp(String email, String password, String name, String surname);
 
 }
