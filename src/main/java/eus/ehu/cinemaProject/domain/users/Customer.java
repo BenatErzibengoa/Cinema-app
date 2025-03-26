e762c658455264c91bdc57812d8e13560ef9e800
vvktria <viktoriand11@gmail.com>
2025-03-26 15:20:37 +0100

    Add constructors to Customer class

--- src/main/java/eus/ehu/cinemaProject/domain/users/Customer.java
+++ src/main/java/eus/ehu/cinemaProject/domain/users/Customer.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -14,4 +15,13 @@ public class Customer extends User {
     private double totalBalance;
     @OneToMany(mappedBy = "customer")
     private List<PurchaseReceipt> purchaseHistory;
+
+    public Customer(String email, String password, String name, String surname) {
+        super(email, password, name, surname);
+        this.points = 0;
+        this.totalBalance = 0;
+        purchaseHistory = new ArrayList<>();
+    }
+
+    public Customer(){}
 }
