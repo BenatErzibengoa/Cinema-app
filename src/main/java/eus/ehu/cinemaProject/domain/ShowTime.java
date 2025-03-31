50cc45d4706b5acf0992439ec5120c7f30ed1035
Beñat <bercibengoa001@ikasle.ehu.eus>
2025-03-31 12:53:20 +0200

    bookSeats method added

--- src/main/java/eus/ehu/cinemaProject/domain/ShowTime.java
+++ src/main/java/eus/ehu/cinemaProject/domain/ShowTime.java
@@ -1,22 +1,41 @@
 package eus.ehu.cinemaProject.domain;
 
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "showtimes")
+
 public class ShowTime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @ManyToOne
+    private ScreeningRoom screeningRoom;
+
     private Date screeningTime;
-    @OneToMany (mappedBy = "showTime")
-    private Set<Seat> seats;
+    @OneToMany
+    private Set<Seat> bookedSeats;
 
     @ManyToOne
     private Film film;
+
+    public ShowTime(ScreeningRoom screeningRoom, Date screeningTime, Film film){
+        this.screeningRoom = screeningRoom;
+        this.screeningTime = screeningTime;
+        this.film = film;
+        this.bookedSeats = new HashSet<>();
+    }
+
+    public void bookSeats(List<Seat> seatsToBook){
+        bookedSeats.addAll(seatsToBook);
+    }
+
+    public ShowTime(){}
 
 }
