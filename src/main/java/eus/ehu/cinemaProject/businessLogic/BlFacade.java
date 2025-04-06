8ca6c5af16cb8edf510c31fbcb1be2e1ce5921fc
Beñat <bercibengoa001@ikasle.ehu.eus>
2025-04-06 12:41:53 +0200

    New queries

--- src/main/java/eus/ehu/cinemaProject/businessLogic/BlFacade.java
+++ src/main/java/eus/ehu/cinemaProject/businessLogic/BlFacade.java
@@ -1,5 +1,12 @@
 package eus.ehu.cinemaProject.businessLogic;
+import eus.ehu.cinemaProject.domain.Film;
+import eus.ehu.cinemaProject.domain.Seat;
+import eus.ehu.cinemaProject.domain.ShowTime;
 import eus.ehu.cinemaProject.domain.users.User;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Interface that specifies the business logic.
@@ -33,6 +40,23 @@ public interface BlFacade {
      * @param surname  the surname
      */
     void signUp(String email, String password, String name, String surname);
+
+    /**
+     * Returns all ShowTimes that matches with the provided date
+     *
+     * @param date    the date
+     */
+    List<ShowTime> getShowTimesByDate(LocalDate date);
+
+    /**
+     * Returns all ShowTimes that matches with the provided date and film
+     *
+     * @param date    the date
+     * @param film    the film
+     */
+    List<ShowTime> getShowTimesByDateAndFilm(LocalDate date, Film film);
+
+
 
 
 
