ce6d997453aad92dacf0d7d58814846abbeae997
vvktria <viktoriand11@gmail.com>
2025-04-07 09:33:53 +0200

    Add ShowTimeController and FXML layout for movie showtimes (not implemented)

---
+++ src/main/java/eus/ehu/cinemaProject/ui/ShowTimeController.java
@@ -0,0 +1,30 @@
+package eus.ehu.cinemaProject.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.TilePane;
+
+public class ShowTimeController {
+
+    @FXML
+    private Label movieDescription;
+
+    @FXML
+    private Label movieDuration;
+
+    @FXML
+    private Label movieGenre;
+
+    @FXML
+    private ImageView movieImage;
+
+    @FXML
+    private Label movieTitle;
+
+    @FXML
+    private TilePane showtimesPane;
+
+
+
+}
