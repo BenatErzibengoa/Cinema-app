cfc5f6094a32ceb36e5cad21b2fb10cae6a40d5a
Beñat <bercibengoa001@ikasle.ehu.eus>
2025-05-02 11:15:14 +0200

    Fix loadContent

--- src/main/java/eus/ehu/cinemaProject/ui/MenuController.java
+++ src/main/java/eus/ehu/cinemaProject/ui/MenuController.java
@@ -81,47 +81,20 @@ public class MenuController {
 
     private void loadContent(String fxmlFile) {
         try {
-            // Retirer la vue du cache si elle existe déjà
-            contentCache.remove(fxmlFile);
-
-
-            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
-            Pane content = loader.load();
-
-            if (loader.getController() instanceof MovieListController) {
-                ((MovieListController) loader.getController()).setBusinessLogic(bl);
-            }
-
-            contentCache.put(fxmlFile, content);
-            contentPane.setCenter(content);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /*
-    private void loadContent(String fxmlFile) {
-        try {
-            contentCache.remove(fxmlFile); //Théo - Remove view if already in cache
             // Check if content is already cached
             Pane content = contentCache.get(fxmlFile);
             if (content == null) {
                 // If not cached, load it and store in cache
                 FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
                 content = loader.load();
-
-                if (loader.getController() instanceof MovieListController) { //Théo
+                contentCache.put(fxmlFile, content);
+                if (loader.getController() instanceof MovieListController) {
                     ((MovieListController) loader.getController()).setBusinessLogic(bl);
                 }
-
-                contentCache.put(fxmlFile, content);
             }
             contentPane.setCenter(content);
-
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    */
 }
